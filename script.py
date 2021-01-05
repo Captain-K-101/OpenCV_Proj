de6e10ef22adebc695bb0f6f83706b69769529b3
Captain-K <44168298+Captain-K-101@users.noreply.github.com>
2021-01-05 15:39:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,38 +1,39 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-import time
 
 
-import cv2
-import numpy as np
-
-image = cv2.imread('mango2.jpg')
+#EDGE DETECTION
+image = cv2.imread('mango37.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 canny = cv2.Canny(blurred, 30, 150)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9, 9))
 dilated = cv2.dilate(canny, kernel)
-result = cv2.bitwise_and(image, image, mask=dilated)
+closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
+result = cv2.bitwise_and(image, image, mask=closing)
 image = cv2.resize(image, (0, 0), fx = 0.5, fy = 0.5)
-dilated = cv2.resize(dilated, (0, 0), fx = 0.5, fy = 0.5)
+closing = cv2.resize(closing, (0, 0), fx = 0.5, fy = 0.5)
 cv2.imshow("Original", image)
-cv2.imshow("Canny", dilated)
+cv2.imshow("Canny", closing)
 result[np.where((result==[0, 0, 0]).all(axis=2))] = [255, 255, 255]
+result[dilated == 0] = 255
 result = cv2.resize(result, (0, 0), fx = 0.5, fy = 0.5)
-cv2.imshow("res", result)
+gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (3, 3),0)
+blurred = cv2.medianBlur(blurred,1)
+gray = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,3)
+
+canny = cv2.Canny(blurred, 30, 150)
+canny = cv2.resize(gray, (0, 0), fx = 2, fy = 2)
+cv2.imshow("res", canny)
 cv2.waitKey(0)
-
-
-
-
-
-
 
 
 
 '''
 
+
+BACKGROUND REMOVAL
 
 img = cv2.imread('mango1.jpg')
 screen_res = 1280, 720
@@ -66,6 +67,7 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 -----------------------------------------------------------------
+
 import cv2
 import numpy as np
 
